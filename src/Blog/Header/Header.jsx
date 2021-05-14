0ddbc0d016145d8bb693214538d08821846729fc
ito <basketball-y.0130@docomo.ne.jp>
2021-05-14 22:24:53 +0900

    ♻️ Blogページのヘッダーのコンポーネント化

---
+++ src/Blog/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import styles from './Header.module.css'
+
+
+export  function Header() {
+  return (
+    <div className={styles.header}>
+      <p>Travel Blog</p>
+    </div>
+  )
+}
