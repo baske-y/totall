7313909262f0769c54a38aed640fe513c275d640
ito <basketball-y.0130@docomo.ne.jp>
2021-05-14 22:46:56 +0900

    ♻️ Blogのメイン部分を追加

---
+++ src/Blog/Main/Main.jsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import Image from 'next/image'
+import styles from './Main.module.css'
+
+
+const ITEMS = [
+  {
+    title: "タイトルテキストテキストテキストテキストテキスト",
+    day: "2020/1/1",
+    category: "カテゴリ1",
+    src:"/img/main1.jpg",
+    text:"本文1テキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+  },
+  {
+    title: "タイトルテキストテキストテキストテキストテキスト",
+    day: "2020/1/2",
+    category: "カテゴリ2",
+    src:"/img/main2.jpg",
+    text:"本文2テキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+  },
+  {
+    title: "タイトルテキストテキストテキストテキストテキスト",
+    day: "2020/1/3",
+    category: "カテゴリ3",
+    src:"/img/main3.jpg",
+    text:"本文3テキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキスト"
+  }
+
+]
+
+export  function Main() {
+  
+  return (
+    <div className={styles.grid}>
+      {ITEMS.map(item => {
+        return (
+          <div className={styles.main}>
+            <h1>{item.title}</h1>
+            <ul>
+              <li className={styles.first}>
+                {item.day}
+              </li>
+              <li>{item.category}</li>
+            </ul>
+            <div>
+          <Image 
+              src={item.src} 
+              loading="eager" 
+              width={700} 
+              height={400} />
+
+            </div>
+            <h2>
+              {item.text}
+            </h2>
+            <Link href="/">
+              <a >READ MORE</a>
+            </Link>
+        </div>
+        )
+      })}
+    </div>
+    
+  )
+}
