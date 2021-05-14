2bdfc817be84b96accfec81536211d2299df2590
ito <basketball-y.0130@docomo.ne.jp>
2021-05-14 22:23:16 +0900

    ♻️ メインページのヘッダーの コンポーネント化

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import styles from './Header.module.css'
+
+
+export  function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>TOTALLY</h1>
+      <div className={styles.header_side}>
+        <ul className={styles.left}>
+          <li>
+            <Link href="/">
+            <a>ALL </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>NEW</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>VINTAGE </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>CATEGORY </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>LOOKBOOK </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/Blog">
+            <a>BLOG </a>
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.right}>
+          <li><a>LOGIN </a></li>
+          <li><a>CONTACT </a></li>
+        </ul>
+
+      </div>
+    </div>
+  )
+}
