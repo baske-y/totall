547361ff35fed77a3dc4d5d761f8f5433cb20381
ito <basketball-y.0130@docomo.ne.jp>
2021-05-15 18:35:45 +0900

    ♻️ リファクタリング

--- src/Blog/Article/Article.jsx
+++ src/Blog/Article/Article.jsx
@@ -2,30 +2,47 @@ import Link from "next/link";
 import styles from './Article.module.css'
 import Image from 'next/image'
 
+
+const ITEMS = [
+  {
+    src:"/img/pickup1.jpg",
+    text:"タイトル1テキスト1テキストテキストテキストテキストテキストテキスト",
+    href:"/",
+    ancoer:"READ MORE"
+  },
+  {
+    src:"/img/pickup2.jpg",
+    text:"タイトル2テキスト2テキストテキストテキストテキストテキストテキスト",
+    href:"/",
+    ancoer:"READ MORE"
+  },
+  {
+    src:"/img/pickup3.jpg",
+    text:"タイトル3テキスト3テキストテキストテキストテキストテキストテキスト",
+    href:"/",
+    ancoer:"READ MORE"
+  }
+  
+]
 export  function Article() {
-  return (
+  return(
     <div className={styles.article}>
-      <article>
-      <Image src="/img/pickup1.jpg" loading="eager" width={350} height={200} />
-      <h2>タイトルテキストテキストテキストテキストテキストテキストテキスト</h2>
-      <Link href = "/">
-        <a >READ MORE</a>
-      </Link>
-      </article>
-      <article>
-      <Image src="/img/pickup2.jpg" loading="eager" width={350} height={200} />
-      <h2>タイトルテキストテキストテキストテキストテキストテキストテキスト</h2>
-      <Link href = "/">
-        <a >READ MORE</a>
-      </Link>
-      </article>
-      <article>
-      <Image src="/img/pickup3.jpg" loading="eager" width={350} height={200} />
-      <h2>タイトルテキストテキストテキストテキストテキストテキストテキスト</h2>
-      <Link href = "/">
-        <a >READ MORE</a>
-      </Link>
-      </article>
+      {ITEMS.map(item => {
+        return (
+          <div>
+            <article>
+            <Image src={item.src} loading="eager" width={350} height={200} />
+            <h2>{item.text}</h2>
+            <Link href = {item.href}>
+              <a >{item.ancoer}</a>
+            </Link>
+            </article>
+          </div>
+        )
+    
+      })}
+
     </div>
+
   )
 }
