416bd05bee45a33d4f84f92cab0073a94426d9d3
ito <basketball-y.0130@docomo.ne.jp>
2021-05-15 18:31:49 +0900

    ♻️ Archiveのコンポーネント化

---
+++ src/Blog/Archive/Archive.jsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import styles from './Archive.module.css'
+const ITEMS = [
+  {
+    day:"2020年1月",
+    src:"/"
+  },
+  {
+    day:"2020年2月",
+    src:"/"
+  },
+  {
+    day:"2020年3月",
+    src:"/"
+  },
+  {
+    day:"2020年4月",
+    src:"/"
+  },
+  {
+    day:"2020年5月",
+    src:"/"
+  },
+  {
+    day:"2020年6月",
+    src:"/"
+  },
+  {
+    day:"2020年7月",
+    src:"/"
+  },
+  {
+    day:"2020年8月",
+    src:"/"
+  },
+  {
+    day:"2020年9月",
+    src:"/"
+  },
+  {
+    day:"2020年10月",
+    src:"/"
+  },
+  {
+    day:"2020年11月",
+    src:"/"
+  },
+  {
+    day:"2020年12月",
+    src:"/"
+  },
+]
+export function Archive () {
+  return (
+   <div>
+     <h1 className={styles.title}>Archive</h1>
+     {ITEMS.map(item => {
+       return(
+         <ul className={styles.list}>
+           <li>
+             <Link href={item.src}>
+              <a>{item.day}</a>
+             </Link>
+           </li>
+           
+         </ul>
+ 
+       )
+     })}
+
+   </div>
+
+  )
+}
