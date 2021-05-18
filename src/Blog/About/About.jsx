28e281b03249654dd8f099e69f1aac03ccebd043
ito <basketball-y.0130@docomo.ne.jp>
2021-05-18 22:35:22 +0900

    💄 見た目の微調整

---
+++ src/Blog/About/About.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import styles from './About.module.css'
+
+const ITEMS = [
+  {
+    text:"プロフィール詳細",
+    src:"#"
+  },
+  {
+    text:"お仕事の依頼",
+    src:"#"
+  },
+  {
+    text:"お問い合わせ",
+    src:"#"
+  }
+]
+export  function About() {
+  return (
+    <div className={styles.about}>
+      <h1>About</h1>
+        <p>
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </p>
+      {ITEMS.map(item => {
+        return(
+          <div className={styles.about_list}>
+            <ul>
+              <li>
+                <Link href={item.src}>
+                  <a>{item.text}</a>
+                </Link>
+              </li>
+            </ul>
+
+          </div>
+          )
+        })}
+    </div>
+    
+  )
+}
