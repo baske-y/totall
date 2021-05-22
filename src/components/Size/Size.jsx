8aa5a1b304ab1c9454d6204733bc08e2b4cad049
ito <basketball-y.0130@docomo.ne.jp>
2021-05-22 16:35:46 +0900

    ♻️ const ITEMS の追加

--- src/components/Size/Size.jsx
+++ src/components/Size/Size.jsx
@@ -1,6 +1,31 @@
 
 import styles from './Size.module.css'
 
+const ITEMS = [
+  {
+    color:"White",
+    size:"S",
+    chest:"50 ~ 80",
+    weist:"50 ~ 80",
+    height:"50 ~ 80"
+  },
+  {
+    color:"Red",
+    size:"M",
+    chest:"80 ~ 110",
+    weist:"80 ~ 110",
+    height:"80 ~ 110"
+  },
+  {
+    color:"Black",
+    size:"L",
+    chest:"110 ~ 140",
+    weist:"110 ~ 140",
+    height:"110 ~ 140"
+  },
+
+]
+
 export  function Size() {
   return (
     <div>
@@ -12,42 +37,21 @@ export  function Size() {
             <th className={styles.quantity}></th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>White</td>
-            <td>S</td>
-            <td>
-              <select name={styles.quantity_s} id="">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>White</td>
-            <td>M</td>
-            <td>
-              <select name={styles.quantity_m} id="">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td>White</td>
-            <td>L</td>
-            <td>
-              <select name={styles.quantity_l} id="">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-              </select>
-            </td>
-          </tr>
-
-        </tbody>
+      {ITEMS.map(item => {
+       return(
+         <tr>
+           <td>{item.color}</td>
+           <td>{item.size}</td>
+           <td>
+             <select name={styles.quantity_s} id="">
+               <option value="1">1</option>
+               <option value="2">2</option>
+               <option value="3">3</option>
+             </select>
+           </td>
+         </tr>
+       )
+     })}
       </table>
       <a className={styles.cart_btn} href="#">ADD TO CART</a>
 
@@ -61,24 +65,17 @@ export  function Size() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>S</th>
-            <td>99 ~ 99</td>
-            <td>99 ~ 99</td>
-            <td>99 ~ 99</td>
-          </tr>
-          <tr>
-            <th>M</th>
-            <td>99 ~ 99</td>
-            <td>99 ~ 99</td>
-            <td>99 ~ 99</td>
-          </tr>
-          <tr>
-            <th>L</th>
-            <td>99 ~ 99</td>
-            <td>99 ~ 99</td>
-            <td>99 ~ 99</td>
-          </tr>
+          {ITEMS.map(item => {
+            return(
+              <tr>
+                <th>{item.size}</th>
+                <td>{item.chest}</td>
+                <td>{item.weist}</td>
+                <td>{item.height}</td>
+              </tr>
+            )
+          })}
+          
         </tbody>
       </table>
     </div>
